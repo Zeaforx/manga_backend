609db21c71bfcf65549c2657df21c07343406daf
zeaforx <ajibcaleb@gmail.com>
2025-06-08 16:38:04 +0100

    gitignore dist directory

--- dist/server.js
+++ dist/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mangaRoute from "./routes/manga.route.js";
+import cors from "cors";
 const app = express();
+app.use(cors());
 app.listen("5000", () => {
     console.log("server started on port 5000");
 });
--- server.ts
+++ server.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import mangaRoute from "./routes/manga.route.js";
 import { getMangaList } from "./controller/manga.controller.js";
+import cors from "cors";
 const app = express();
+
+app.use(cors());
 
 app.listen("5000", () => {
   console.log("server started on port 5000");
